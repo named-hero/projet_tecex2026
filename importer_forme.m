dc4873f06a0a083df3d7c1eda1e1e82ba6507373
named-hero <olivier-4.st-jean@polymtl.ca>
2026-09-20 13:33:12 -0400

    Simplification importation forme et ajout commentaire
    - Ajout d'une fonction pour sélectionner un fichier
    - Enregistrement image dans pour un backlog
    - Ajout de commentaire pour mieux comprendre le code

--- importer_forme.m
+++ importer_forme.m
@@ -1,8 +1,14 @@
 clear
-pixels = imread("projet_tecex2026\Formes (png)\Forme test.png");
 
+%% Choisir un fichier
+[file, folder] = uigetfile('*.png');
+file_location = string(folder) + string(file);
+pixels = imread(file_location);
+
+%% Crop image pour cadrer parfaitement la forme
+%Identification de tout les points blanc
 if ndims(pixels) == 3
-    mask = any(pixels == 255, 3);   % for a white background
+    mask = any(pixels == 255, 3);
 else
     mask = pixels == 255;
 end
@@ -13,16 +19,24 @@ if isempty(rowIdx)
     error("No object found in the image.");
 end
 
+% Sélection des bordure de la forme
 topRow = min(rowIdx);
 bottomRow = max(rowIdx);
 leftCol = min(colIdx);
 rightCol = max(colIdx);
 
+%Crop de l'image
 croppedPixels = pixels(topRow:bottomRow, leftCol:rightCol, :);
 
-imshow(croppedPixels)
+imshow(croppedPixels) % Permet de visulaliser l'image
 
-croppedPixels(croppedPixels > 0) = 1;
+%% Dernières actions et enregistrement
+croppedPixels(croppedPixels > 0) = 1; % Remplacer pixels non noir par valeur 1
+
 shape = croppedPixels;
 nom = inputdlg("Quelle est le nom de la forme");
+
+%Enregistrer le fichier image initial pour backlog
+copyfile(file_location, append("projet_tecex2026\Formes (png)\", nom, ".png"))
+%Enregistrer nouveau fichier dans variable <shape>
 save(append("projet_tecex2026\Formes enregistrer\", nom), "shape")
